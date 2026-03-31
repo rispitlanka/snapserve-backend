38d4c7edaee48c32ec40e0e2916ed06244c96221
SANTA <it22113740@my.sliit.lk>
2026-03-31 16:48:57 +0530

    Enable CORS in the main application bootstrap for all origins and specified methods and headers.

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE','PATCH','OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
